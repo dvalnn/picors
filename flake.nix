488fe99ab605319338aee4b7ce0ce32ca95cb533
dvalinn <tiago.andre.amorim@gmail.com>
2024-09-01 16:17:35 +0100

    first

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    fenix.url = "github:nix-community/fenix";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    naersk,
+    nixpkgs,
+    fenix,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = (import nixpkgs) {
+          inherit system;
+          overlays = [fenix.overlays.default];
+        };
+
+        naersk' = pkgs.callPackage naersk {};
+      in rec {
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+        };
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            alejandra
+            rust-analyzer
+            (pkgs.fenix.fromToolchainFile{
+              file = ./rust-toolchain.toml;
+              sha256 = "sha256-3jVIIf5XPnUU1CRaTyAiO0XHVbJl12MSx3eucTXCjtE=";
+            })
+
+            # project specific
+            elf2uf2-rs # used for loading uf2 over usb
+            flip-link # stack overflow protection
+          ];
+        };
+      }
+    );
+}
