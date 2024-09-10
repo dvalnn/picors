175b52c163f93e8983025569d99ca225b344db96
dvalinn <tiago.andre.amorim@gmail.com>
2024-09-10 22:34:52 +0100

    multicore demo

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,22 @@
 
 #![no_std]
 #![no_main]
+#![allow(unused)]
 
-use core::str;
+use cyw43::Control;
+use embassy_rp::{
+    bind_interrupts,
+    gpio::{Level, Output},
+    multicore::{self, spawn_core1},
+    peripherals::{DMA_CH0, PIO0},
+    pio::{InterruptHandler, Pio},
+};
 
-use embassy_rp::bind_interrupts;
-use embassy_rp::gpio::{Level, Output};
-use embassy_rp::peripherals::{DMA_CH0, PIO0};
-use embassy_rp::pio::{InterruptHandler, Pio};
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel};
 
-use embassy_executor::Spawner;
 use embassy_net::Stack;
 use embassy_time::{Duration, Timer};
+use embassy_executor::{Executor, Spawner};
 
 use cyw43_pio::PioSpi;
 use static_cell::StaticCell;
@@ -28,6 +33,112 @@ bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => InterruptHandler<PIO0>;
 });
 
+static CYW43_CONTROL: StaticCell<Control> = StaticCell::new();
+
+static mut CORE1_STACK: multicore::Stack<4069> = multicore::Stack::new();
+static EXECUTOR_C1: StaticCell<Executor> = StaticCell::new();
+static CHANNEL: Channel<CriticalSectionRawMutex, LedState, 1> = Channel::new();
+
+enum LedState {
+    On,
+    Off,
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    info!("Hello World!");
+
+    // Initialization and setup
+
+    let p = embassy_rp::init(Default::default());
+    // let fw = include_bytes!("../cyw43-firmware/43439A0.bin");
+    // let clm = include_bytes!("../cyw43-firmware/43439A0_clm.bin");
+
+    // To make flashing faster for development, you may want to flash the firmwares independently
+    // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
+    //     probe-rs download 43439A0.bin --binary-format bin --chip RP2040 --base-address 0x10100000
+    //     probe-rs download 43439A0_clm.bin --binary-format bin --chip RP2040 --base-address 0x10140000
+    let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 230321) };
+    let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
+
+    let pwr = Output::new(p.PIN_23, Level::Low);
+    let cs = Output::new(p.PIN_25, Level::High);
+
+    let mut pio = Pio::new(p.PIO0, Irqs);
+    let spi = PioSpi::new(
+        &mut pio.common,
+        pio.sm0,
+        pio.irq0,
+        cs,
+        p.PIN_24,
+        p.PIN_29,
+        p.DMA_CH0,
+    );
+
+    static STATE: StaticCell<cyw43::State> = StaticCell::new();
+    let state = STATE.init_with(cyw43::State::new);
+    let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    unwrap!(spawner.spawn(cyw43_task(runner)));
+
+    control.init(clm).await;
+    control
+        .set_power_management(cyw43::PowerManagementMode::PowerSave)
+        .await;
+
+    let control = CYW43_CONTROL.init(control);
+
+    // WIFI Scan
+    {
+        info!("Starting WIFI scan");
+        let mut scanner = control.scan(Default::default()).await;
+        while let Some(bss) = scanner.next().await {
+            if let Ok(ssid_str) = core::str::from_utf8(&bss.ssid) {
+                info!("scanned {} = {:x}", ssid_str, bss.bssid);
+            }
+        }
+    }
+
+    spawn_core1(
+        p.CORE1,
+        unsafe { &mut *core::ptr::addr_of_mut!(CORE1_STACK) },
+        move || {
+            EXECUTOR_C1
+                .init_with(Executor::new)
+                .run(|spawner| unwrap!(spawner.spawn(core1_task())))
+        },
+    );
+
+    info!("Starting Core 0 Loop");
+    loop {
+        match CHANNEL.receive().await {
+            LedState::On => {
+                control.gpio_set(0, true).await;
+                info!("led on!");
+            }
+            LedState::Off => {
+                control.gpio_set(0, false).await;
+                info!("led off!");
+            }
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn core1_task() {
+    info!("Hello from core 1!");
+    let delay = Duration::from_secs(1);
+    loop {
+        CHANNEL.send(LedState::On).await;
+        info!("led on!");
+        Timer::after(delay).await;
+        CHANNEL.send(LedState::Off).await;
+        info!("led off!");
+        Timer::after(delay).await;
+    }
+}
+
+////// BACKGROUND TASKS -- SECTION BEGIN
+
 #[embassy_executor::task]
 async fn cyw43_task(
     runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>,
@@ -40,71 +151,4 @@ async fn net_task(stack: &'static Stack<cyw43::NetDriver<'static>>) -> ! {
     stack.run().await
 }
 
-#[embassy_executor::main]
-async fn main(spawner: Spawner) {
-    info!("Hello World!");
-
-    // Initialization and setup
-    let mut control = {
-        let p = embassy_rp::init(Default::default());
-        // let fw = include_bytes!("../cyw43-firmware/43439A0.bin");
-        // let clm = include_bytes!("../cyw43-firmware/43439A0_clm.bin");
-
-        // To make flashing faster for development, you may want to flash the firmwares independently
-        // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
-        //     probe-rs download 43439A0.bin --binary-format bin --chip RP2040 --base-address 0x10100000
-        //     probe-rs download 43439A0_clm.bin --binary-format bin --chip RP2040 --base-address 0x10140000
-        let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 230321) };
-        let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
-
-        let pwr = Output::new(p.PIN_23, Level::Low);
-        let cs = Output::new(p.PIN_25, Level::High);
-
-        let mut pio = Pio::new(p.PIO0, Irqs);
-        let spi = PioSpi::new(
-            &mut pio.common,
-            pio.sm0,
-            pio.irq0,
-            cs,
-            p.PIN_24,
-            p.PIN_29,
-            p.DMA_CH0,
-        );
-
-        static STATE: StaticCell<cyw43::State> = StaticCell::new();
-        let state = STATE.init_with(|| {
-            cyw43::State::new()
-        });
-        let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
-        unwrap!(spawner.spawn(cyw43_task(runner)));
-
-        control.init(clm).await;
-        control
-            .set_power_management(cyw43::PowerManagementMode::PowerSave)
-            .await;
-
-        control
-    };
-
-    // WIFI Scan
-    {
-        let mut scanner = control.scan(Default::default()).await;
-        while let Some(bss) = scanner.next().await {
-            if let Ok(ssid_str) = str::from_utf8(&bss.ssid) {
-                info!("scanned {} = {:x}", ssid_str, bss.bssid);
-            }
-        }
-    }
-
-    // Builtin Led Blinky
-    let delay = Duration::from_secs(1);
-    loop {
-        info!("led on!");
-        control.gpio_set(0, true).await;
-        Timer::after(delay).await;
-
-        info!("led off!");
-        control.gpio_set(0, false).await;
-        Timer::after(delay).await;
-    }
-}
+////// BACKGROUND TASKS -- SECTION END
